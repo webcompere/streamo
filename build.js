eddfc084ba8acaf8285282d7ad12156ade8af342
Ashley Frieze <ashley@incredible.org.uk>
2025-04-18 10:44:09 +0100

    Configure ESBuild

---
+++ build.js
@@ -0,0 +1,28 @@
+const esbuild = require('esbuild');
+const { execSync } = require('child_process');
+
+const sharedConfig = {
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  sourcemap: true,
+  minify: false,
+  target: ['es2020'],
+  external: [], // add dependencies you want to exclude here
+};
+
+// Build ESM
+esbuild.build({
+  ...sharedConfig,
+  format: 'esm',
+  outfile: 'build/index.mjs',
+});
+
+// Build CJS
+esbuild.build({
+  ...sharedConfig,
+  format: 'cjs',
+  outfile: 'build/index.cjs',
+});
+
+// Emit TypeScript declaration files
+execSync('tsc --emitDeclarationOnly', { stdio: 'inherit' });
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './Stream';
+export type * from './functions';
+export * from './functions';
+export * from './Optional';
