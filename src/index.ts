bd144052cced693b2ebc6fd702359ee64d155097
Ashley Frieze <ashley@incredible.org.uk>
2025-04-21 12:41:27 +0100

    Add exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './Stream';
 export type * from './functions';
 export * from './functions';
 export * from './Optional';
+export type * from './Collectors';
+export * from './Collectors';
+export * from './Iterables';
+export type * from './Iterables';
